5c7f4444d6c69560c15acebaff44d84c9e95af49
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-02 01:34:13 +0100

    edit text of official app modal

--- components/redirectNotificationModal.js
+++ components/redirectNotificationModal.js
@@ -10,8 +10,8 @@ export default function RedirectNotificationModal({dismissFunction}) {
         <h1 className={styles.modalTitle}>
           Obavijest
         </h1>
-        <p><strong>U međuvremenu je objavljena i službena aplikacija</strong> iza koje stoji Geodetski fakultet Sveučilišta u Zagrebu, hrvatska OpenStreetMap zajednica i Open IT d.o.o. u suradnji s Hrvatskom gorskom službom spašavanja, Operativnim centrom Civilne zaštite i Državnom geodetskom upravom Republike Hrvatske pripremili su i održavaju platformu na kojoj možete zatražiti i ponuditi pomoć i informacije.</p>
-        <p>Kako bi izbjegli konfuziju s objavljivanjem na više aplikacija, pozivamo Vas da koristite službenu aplikaciju. <strong>Kliknite na gumb da posjetite službenu aplikaciju.</strong></p>
+        <p><strong>U međuvremenu je objavljena i službena aplikacija</strong>.</p>
+        <p>Klikom na gumb ispod možete otići na službenu aplikaciju, no možete nastaviti koristiti i ovu ukoliko tako preferirate.</p>
         <a className={styles.modalButton} href='https://potres2020.openit.hr'>Idi na službenu aplikaciju</a>
       </div>
     </div>
