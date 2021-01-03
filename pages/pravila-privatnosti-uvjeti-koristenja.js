c4c6c3a9440f83b82c92d43775ecd1f84b3533c0
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-03 05:19:55 +0100

    add some privacy and terms to activate facebook

---
+++ pages/pravila-privatnosti-uvjeti-koristenja.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import styles from './index.module.scss'
+import MainSiteLayout from 'layouts/mainSiteLayout'
+import LayoutWithSideMap from 'layouts/layoutWithSideMap'
+
+export default function Terms() {
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pomoć žrtvama potresa | Pravila privatnosti i uvjeti korištenja</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBza8tAYUna_mtCXdstnhu50rJXJ7bi5yw&libraries=places"></script>
+      </Head>
+      <MainSiteLayout>
+        {notificationModalActive && (
+          <RedirectNotificationModal dismissFunction={() => setNotificationModalActive(false)} />
+        )}
+
+        <LayoutWithSideMap items = {mapItems}>
+          <div className={styles.introSection}>
+            <h1>Pravila privatnosti i uvjeti korištenja</h1>
+            <p>Ova aplikacija je razvijena u svrhu koordinacije volontera za potrebe odgovora na prirodnu katastrofu koja je pogodila središnju Hrvatsku.</p>
+            <p>Podaci o korisnicima se ne prikupljaju, izuzev u svrhu praćenja performansi aplikacije, te dodjele određenih zadataka pojedinim volonterima.</p>
+            <p>Aplikacija koristi Facebook Login sustav, no korisničke podatke ne sprema u bazu podataka.</p>
+          </div>
+        </LayoutWithSideMap>
+      </MainSiteLayout>
+    </div>
+  )
+}
