33f7a16053687b0a6b8ed387086c9f9c2e2176bb
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-03 16:22:15 +0100

    testing the actual hook

--- pages/api/potres2020-hook.js
+++ pages/api/potres2020-hook.js
@@ -1,5 +1,49 @@
+const baseUrl = process.env.BASE_URL || 'https://lit-coast-00350.herokuapp.com'
+
+const findLatLonData = (values, requestedValue) => {
+  let value = null;
+  Object.keys(values).forEach((key) => {
+    if ((Array.isArray(values[key])) && (values[key][0][requestedValue])) {
+      value = values[key][0][requestedValue];
+    }
+  })
+  return value;
+}
+
+export async function addEntry(path, data) {
+  const url = `${baseUrl}/${path}`
+
+  const response = await fetch(`${url}`, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .catch( (error) => {
+      return error
+    })
+  const payload = await response.json()
+  return payload
+}
+
 export default (req, res) => {
   console.log(req.body);
-  res.statusCode = 200
+  const item = req.body;
+  const newData = {
+    'locationLat': findLatLonData(item.values, 'lat'),
+    'locationLon': findLatLonData(item.values, 'lon'),
+    'location': item.title,
+    'description': item.content,
+    'notes': `URL na izvorni item na Potres2020: ${item.url}     Dodatni info iz Potres2020: ${item.values['3c8441b3-5744-48bb-9d9e-d6ec4be50613']}`,
+    'contact_phone': item.values['4583d2a1-331a-4da2-86df-3391e152198e'],
+    'contact_name': item.values['1328cf24-09de-44cd-b159-6242e6165530'],
+    'submitter_email': 'potres@2020.hr',
+    'available_on_whatsapp': false,
+  }
+  addEntry(newData.type, newData)
+    .then((response) => res.statusCode = 200)
+    .catch((error) => console.log('Error in Potres2020 webhook:', error))
   res.json({ content: 'Thanks!' })
 }
