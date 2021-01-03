abbf2743df973b94327187a2bb003130f510e04e
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-03 04:27:27 +0100

    another try at env vars

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
-const baseUrl = 'https://lit-coast-00350.herokuapp.com'
-// const baseUrl = process.env.BASE_URL || 'http://localhost:1337'
+// const baseUrl = 'https://lit-coast-00350.herokuapp.com'
+const baseUrl = process.env.BASE_URL || 'http://localhost:1337'
 
 export async function fetchQuery(path, params = null) {
   let url
