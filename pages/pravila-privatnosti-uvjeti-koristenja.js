08e8324bfaa6008dacf35214af360e6da3eab5fb
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-03 05:25:43 +0100

    need sleep.stupid mistakes.

--- pages/pravila-privatnosti-uvjeti-koristenja.js
+++ pages/pravila-privatnosti-uvjeti-koristenja.js
@@ -13,11 +13,8 @@ export default function Terms() {
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBza8tAYUna_mtCXdstnhu50rJXJ7bi5yw&libraries=places"></script>
       </Head>
       <MainSiteLayout>
-        {notificationModalActive && (
-          <RedirectNotificationModal dismissFunction={() => setNotificationModalActive(false)} />
-        )}
 
-        <LayoutWithSideMap items = {mapItems}>
+        <LayoutWithSideMap items = {[]}>
           <div className={styles.introSection}>
             <h1>Pravila privatnosti i uvjeti korištenja</h1>
             <p>Ova aplikacija je razvijena u svrhu koordinacije volontera za potrebe odgovora na prirodnu katastrofu koja je pogodila središnju Hrvatsku.</p>
