a8502684a1b9ba354254ad970816ac0996ec4700
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-07 22:00:51 +0100

    redirect /admin to admin

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   env: {
     customKey: 'my-value',
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://lit-coast-00350.herokuapp.com/admin',
+        permanent: true,
+      },
+    ]
+  },
 }
