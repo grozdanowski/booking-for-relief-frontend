dd89e649f75d27296a8e32e4c6257f70f859a25a
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-10 14:11:11 +0100

    add a floating action button to create an entry on mobile

--- layouts/mainSiteLayout.js
+++ layouts/mainSiteLayout.js
@@ -1,5 +1,7 @@
 import Header from 'components/header'
 import styles from './mainSiteLayout.module.scss'
+import { Add } from '@material-ui/icons'
+import Link from 'next/link'
 
 export default function MainSiteLayout({ filterValue, setLocationFilterFunction, itemTags = [], children }) {
 
@@ -9,6 +11,11 @@ export default function MainSiteLayout({ filterValue, setLocationFilterFunction,
         <Header itemTags = {itemTags} />
       </section>
       {children}
+      <Link href='/dodaj-unos'>
+        <a className={styles['floating-action-button']}>
+          <Add />
+        </a>
+      </Link>
       <footer className={styles.appFooter}>
         Aplilkacija je work in progress i kontinuirano se radi na unaprijeđenju. Za sve probleme i upite slobodno se javite na <a href="mailto:matej@bytepanda.io">matej@bytepanda.io</a>.
       </footer>
