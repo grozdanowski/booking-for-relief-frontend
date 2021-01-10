724a94919a04a2f78d43031376252cebf97e334c
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-10 16:01:19 +0100

    properly set up env variables for the frontend

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  serverRuntimeConfig: {
+    baseUrl: process.env.BASE_URL,
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    emitVolunteerAssignedHook: process.env.EMIT_VOLUNTEER_ASSIGNED_HOOK,
+    emitVolunteerDoneHook: process.env.EMIT_VOLUNTEER_DONE_HOOK,
+  },
+  publicRuntimeConfig: {
+    baseUrl: process.env.BASE_URL,
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    emitVolunteerAssignedHook: process.env.EMIT_VOLUNTEER_ASSIGNED_HOOK,
+    emitVolunteerDoneHook: process.env.EMIT_VOLUNTEER_DONE_HOOK,
+  },
   env: {
     customKey: 'my-value',
   },
