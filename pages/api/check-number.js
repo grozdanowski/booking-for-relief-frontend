65b5755a2ac5f58e71959fd13f5f1900ba0c2678
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-14 22:25:02 +0100

    remove console logs

--- pages/api/check-number.js
+++ pages/api/check-number.js
@@ -1,9 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
-  // res.statusCode = 200
-  // res.json({ key: process.env.INFOBIP_API_KEY, body: req.body })
-  console.log(req.body)
   try {
     const response = await fetch(`${process.env.INFOBIP_API_BASE_URL}/number/1/query`, {
         method: 'post',
@@ -14,10 +11,8 @@ export default async (req, res) => {
         body: JSON.stringify(req.body)
       })
     const payload = await response.json()
-    console.log(payload, `${process.env.INFOBIP_API_BASE_URL}/number/1/query`, `App ${process.env.INFOBIP_API_KEY}`)
     res.json(payload)
   } catch (error) {
-    console.log(error)
     res.statusCode = 500
     res.json({ error: error })
   }
